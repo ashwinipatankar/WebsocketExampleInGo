66b8cf4c0d7278545b3c145d520f492f83886778
Ashwini Patankar <ashwinispatankar@gmail.com>
2017-04-12 19:45:42 +0530

    Added better time calculation but limited to five threads only

--- main.go
+++ main.go
@@ -34,22 +34,35 @@ func main() {
 	var messageCounter uint64
 	var tNow time.Time
 	var tCurrent time.Duration
+	var threadId [5]int
+	var threadIndex int
+	var threadMsgCount [5]uint64
+	var threadMsgTime [5]time.Duration
+	var threadMsgStartTime [5]time.Time
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		var conn, _ = upgrader.Upgrade(w, r, nil)
 
 		go func(conn *websocket.Conn) {
+			var threadIndexForCounter = threadIndex
+			threadId[threadIndexForCounter] = threadIndex
+			threadIndex++
 			tNow = time.Now()
+			threadMsgStartTime[threadIndexForCounter] = time.Now()
 			for {
 				mType, msg, err := conn.ReadMessage()
 				if err != nil {
 					tCurrent = time.Since(tNow)
-					fmt.Println("Before Closing connection Total number of requests received: ", messageCounter, "In time: ", tCurrent)
+					threadMsgTime[threadIndexForCounter] = time.Since(threadMsgStartTime[threadIndexForCounter])
+					fmt.Println("Before Closing connection Total number of requests received: ", messageCounter, "In time: ", tCurrent, mType)
+					fmt.Println("TotalThreads:", threadIndex)
 					conn.Close()
 					return
 				}
 				messageCounter++
-				fmt.Println(mType, string(msg))
+				threadMsgCount[threadIndexForCounter]++
+				//fmt.Println(mType, string(msg))
+				fmt.Println("MsgSize ", len(msg), "Message number ", messageCounter, "Time Elapsed: ", time.Since(tNow))
 				//fmt.Println("Total number of requests received: ", messageCounter, "In time: ", time.Since(tNow))
 				/*
 					err = conn.WriteMessage(mType, msg)
@@ -83,6 +96,12 @@ func main() {
 		log.Println("Stopping Server ...")
 		tCurrent = time.Since(tNow)
 		fmt.Println("Total number of requests received: ", messageCounter, "In time: ", tCurrent)
+		fmt.Println("Total Threads ", threadIndex)
+
+		for index := 0; index <= threadIndex; index++ {
+			threadMsgTime[index] = time.Since(threadMsgStartTime[index])
+			fmt.Println("Thread Index: ", index, " Message Count: ", threadMsgCount[index], "Time Elapsed: ", threadMsgTime[index])
+		}
 		os.Exit(1)
 	}()
 	log.Fatal(http.ListenAndServe(":9000", nil))
