7cd9a7122be49fbcac528badb7d6fcddee8809af
Ashwini Patankar <ashwinispatankar@gmail.com>
2017-04-12 19:05:23 +0530

    Message counter added, this code can be improved a lot

--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
 	"github.com/gorilla/websocket"
@@ -23,25 +28,38 @@ type myStruct struct {
 }
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	/*http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "index.html")
-	})
+	})*/
+	var messageCounter uint64
+	var tNow time.Time
+	var tCurrent time.Duration
 
-	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		var conn, _ = upgrader.Upgrade(w, r, nil)
 
 		go func(conn *websocket.Conn) {
+			tNow = time.Now()
 			for {
 				mType, msg, err := conn.ReadMessage()
 				if err != nil {
 					conn.Close()
 					return
 				}
-				conn.WriteMessage(mType, msg)
+				messageCounter++
+				fmt.Println(mType, string(msg))
+				fmt.Println("Total number of requests received: ", messageCounter, "In time: ", time.Since(tNow))
+				/*
+					err = conn.WriteMessage(mType, msg)
+					if err != nil {
+						tCurrent = time.Since(tNow)
+						fmt.Println("Total number of requests received: ", messageCounter, "In time: ", tCurrent)
+					}*/
 			}
-
+			tCurrent = time.Since(tNow)
+			fmt.Println("Total number of requests received: ", messageCounter, "In time: ", tCurrent)
 		}(conn)
-
+		fmt.Println("Total number of requests received: ", messageCounter, "In time: ", tCurrent)
 		go func(conn *websocket.Conn) {
 			ch := time.Tick(5 * time.Second)
 
@@ -53,6 +71,18 @@ func main() {
 		}(conn)
 
 	})
+	//handle server interrupts
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	signal.Notify(c, syscall.SIGTERM)
 
-	http.ListenAndServe(":9000", nil)
+	go func() {
+		<-c
+		log.Println("Stopping Server ...")
+		tCurrent = time.Since(tNow)
+		fmt.Println("Total number of requests received: ", messageCounter, "In time: ", tCurrent)
+		os.Exit(1)
+	}()
+	log.Fatal(http.ListenAndServe(":9000", nil))
+	//	http.ListenAndServe(":9000", nil)
 }
